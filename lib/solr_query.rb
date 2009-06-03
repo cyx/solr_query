af5f1627f4d50b853db70aedf821fff300dba796
Cyril David <cyx.ucron@gmail.com>
2009-06-03 12:34:04 +0800

    initial commit

---
+++ lib/solr_query.rb
@@ -0,0 +1,78 @@
+require 'activesupport'
+
+class SolrQuery
+  AND = ' AND '
+  OR  = ' OR '
+  
+  def self.escape( str )
+    str.to_s.gsub(/([!+\-\(\)\{\}\[\]^\"\~\*\?\:\\])/, '\\\\\1')
+  end
+  
+  def self.create( operator = AND )
+    solr_query = new
+    yield solr_query
+    solr_query.to_s( operator )
+  end
+  
+  attr_reader :conditions
+  
+  def initialize
+    @conditions = []
+    @stream     = [ [] ]
+  end
+  
+  def union( &block )
+    do_operation( OR, &block )
+  end
+  
+  def intersection( &block )
+    do_operation( AND, &block )
+  end
+  
+  def push( condition )
+    stream.push( condition )
+  end
+  
+  def stream
+    @stream[-1]
+  end
+  
+  def condition( field, value, escaping = true )
+    return if value.blank?
+    stream.push("#{field}:#{escape(value, escaping)}")
+  end
+  
+  def term( value, escaping = true )
+    return if value.to_s.strip.blank?
+    stream.push( escape(value, escaping) )
+  end
+  
+  def like( value, escaping = true )
+    "#{escape(value.downcase, escaping)}*"
+  end
+  
+  def to_s( operator = AND )
+    stream.join( operator )
+  end
+  
+  protected
+    def parenthesize( str )
+      "( #{str} )"
+    end
+    
+    def do_operation( op )
+      solr_query = SolrQuery.new
+      @stream    << solr_query
+      yield
+      generated = @stream.pop.to_s( op )
+      stream.push(parenthesize(generated)) if generated.present?
+    end
+    
+    def escape( str, escaping = true )
+      if escaping
+        self.class.escape( str )
+      else
+        str
+      end
+    end
+end
--- test/solr_query_test.rb
+++ test/solr_query_test.rb
@@ -1,7 +1,156 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/test_helper'
 
 class SolrQueryTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "given term 'quick brown'" do
+    should "be quick brown" do
+      result = SolrQuery.create do |q|
+        q.term 'quick brown'
+      end
+      
+      assert_equal 'quick brown', result
+    end
+  end
+  
+  context "given term ' '" do 
+    should "be ''" do
+      result = SolrQuery.create { |q| q.term ' ' }
+      assert_equal '', result
+    end
+  end
+  
+  context "given term '' condition 'category_id', nil" do
+    should " be '' " do
+      result = SolrQuery.create do |q|
+        q.term ''
+        q.condition 'category_id', nil
+      end
+      
+      assert_equal '', result
+    end
+  end
+  # GIVEN: 
+  #
+  # SolrQuery.create do |q|
+  #     q.union {
+  #       q.condition 'allowed', 'all'
+  # 
+  #       q.union {
+  #         q.condition 'allowed', 'contacts'
+  #         q.condition 'poster_contact_ids', 1
+  #       }
+  #     }
+  #   end
+  context "given the query above" do 
+    should "be allowed:all OR (allowed:contacts OR poster_contact_ids:1)" do
+      result = SolrQuery.create do |q|
+        q.union {
+          q.condition 'allowed', 'all'
+  
+          q.union {
+            q.condition 'allowed', 'contacts'
+            q.condition 'poster_contact_ids', 1
+          }
+        }
+      end
+      
+      assert_equal '( allowed:all OR ( allowed:contacts OR poster_contact_ids:1 ) )', result
+    end
+  end
+  
+  context "given condition 'allowed', 'all'" do
+      should "be allowed:all" do
+        result = SolrQuery.create do |q|
+          q.condition 'allowed', 'all'
+        end
+        assert_equal 'allowed:all', result
+      end
+    end
+    
+    context "given condition 'allowed', 'all' then condition 'poster_contact_ids', 1" do
+      should "be allowed:all AND poster_contact_ids:1" do
+        result = SolrQuery.create do |q|
+          q.condition 'allowed', 'all'
+          q.condition 'poster_contact_ids', 1
+        end
+        
+        assert_equal 'allowed:all AND poster_contact_ids:1', result
+      end
+    end
+    
+  context "given condition 'allowed', 'all' union { condition 'poster_contact_ids', 1 condition 'hoy', 'hey' }" do
+    should "be allowed:all AND (poster_contact_ids:1 OR hoy:hey)" do
+      result = SolrQuery.create do |q|
+        q.condition 'allowed', 'all'
+        q.union {
+          q.condition 'poster_contact_ids', 1
+          q.condition 'hoy', 'hey'
+        }
+      end
+      
+      assert_equal 'allowed:all AND ( poster_contact_ids:1 OR hoy:hey )', result
+    end
+  end
+
+  context "given condition 'allowed', 'all' intersection { condition 'poster_contact_ids', 1 condition 'hoy', 'hey' }" do
+    should "be allowed:all AND (poster_contact_ids:1 AND hoy:hey)" do
+      result = SolrQuery.create do |q|
+        q.condition 'allowed', 'all'
+        q.intersection {
+          q.condition 'poster_contact_ids', 1
+          q.condition 'hoy', 'hey'
+        }
+      end
+      
+      assert_equal 'allowed:all AND ( poster_contact_ids:1 AND hoy:hey )', result
+    end
+  end
+  
+  #   union {
+  #     set :allowed, 'all'
+  #   
+  #     intersection {
+  #       set :allowed, 'contacts'
+  #       set :poster_contact_ids, 1
+  #     }
+  #   
+  #     union {
+  #       set :allowed, 1
+  #       set :allowed, 2
+  #     }
+  #   }
+  #   
+  context "given the pseudo code above" do
+    should "be (allowed:all OR (allowed:contacts AND poster_contact_ids:1) OR (allowed:1 OR allowed:2)" do
+      result = SolrQuery.create do |q|
+        q.union {
+          q.condition 'allowed', 'all'
+          
+          q.intersection {
+            q.condition 'allowed', 'contacts'
+            q.condition 'poster_contact_ids', 1
+          }
+          
+          q.union {
+            q.condition 'allowed', 1
+            q.condition 'allowed', 2
+          }
+        }
+      end
+      
+      assert_equal '( allowed:all OR ( allowed:contacts AND poster_contact_ids:1 ) OR ( allowed:1 OR allowed:2 ) )', result
+    end
+  end
+  
+  context "given condition 'allowed', 'all' intersection { }" do
+    should "be allowed:all" do
+      result = SolrQuery.create do |q|
+        q.condition 'allowed', 'all'
+        q.intersection {
+          
+        }
+      end
+      
+      assert_equal 'allowed:all', result
+    end
   end
 end
